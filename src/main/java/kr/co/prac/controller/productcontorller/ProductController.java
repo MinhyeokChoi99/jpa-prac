ebdf6e24455b78f605a51c3cd5506e6584b71fa0
minhyeokChoi <bbbim5656@gmail.com>
2026-06-19 16:14:17 +0900

    productController

--- src/main/java/kr/co/prac/controller/productcontorller/ProductController.java
+++ src/main/java/kr/co/prac/controller/productcontorller/ProductController.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.prac.dto.product.ProductResponse;
-import kr.co.prac.repository.ProductRepository;
 import kr.co.prac.service.productService.ProductService;
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ProductController {
 	
