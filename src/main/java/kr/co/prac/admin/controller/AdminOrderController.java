324bd6e54a7585a37a886f9509f7b39a13264a5f
Minhyeok Choi <bbbim5656@gmail.com>
2026-06-25 00:30:00 +0900

    refactor: add admin orderController

--- src/main/java/kr/co/prac/admin/controller/AdminOrderController.java
+++ src/main/java/kr/co/prac/admin/controller/AdminOrderController.java
@@ -1,7 +1,25 @@
 package kr.co.prac.admin.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.prac.global.session.SessionUtil;
+import kr.co.prac.orders.dto.OrderResponse;
+import kr.co.prac.orders.repository.OrdersRepository;
+import kr.co.prac.orders.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class AdminOrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/admin/orders")
+    public List<OrderResponse> ordersList(HttpServletRequest httpServletRequest) {
+        SessionUtil.requireAdmin(httpServletRequest);
+        return orderService.findAll();
+    }
 }
--- src/main/java/kr/co/prac/orders/dto/OrderResponse.java
+++ src/main/java/kr/co/prac/orders/dto/OrderResponse.java
@@ -9,14 +9,12 @@ import lombok.Getter;
 @Getter
 public class OrderResponse {
 	private Long number;
-//	private Member member; 엔티티가 딸려가면 X
 	private Long memberId;
 	private LocalDateTime orderDate;
 	private OrderStatus status;
 	
 	public OrderResponse(Orders orders) {
 		this.number = orders.getNumber();
-//		this.member = orders.getMember();
 		this.memberId = orders.getMember().getNumber();
 		this.orderDate = orders.getOrderDate();
 		this.status = orders.getStatus();
